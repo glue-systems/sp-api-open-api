60705402923a7f65284ded9c2d57118ff61afe50
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-07-25 23:27:55 +0000

    Fleshing out ListingsApiTest

--- tests/Clients/ListingsItems/Api/ListingsApiTest.php
+++ tests/Clients/ListingsItems/Api/ListingsApiTest.php
@@ -5,6 +5,7 @@ namespace Tests\Clients\ListingsItems\Api;
 use Glue\SPAPI\OpenAPI\Clients\ListingsItems\Model\ListingsItemPatchRequest;
 use Glue\SPAPI\OpenAPI\Clients\ListingsItems\Model\ListingsItemPutRequest;
 use Glue\SPAPI\OpenAPI\Clients\ListingsItems\Model\ListingsItemSubmissionResponse;
+use Glue\SPAPI\OpenAPI\Clients\ListingsItems\Model\PatchOperation;
 use Glue\SPAPI\OpenAPI\Services\Factory\ClientFactory;
 use Tests\TestCase;
 
@@ -32,51 +33,68 @@ class ListingsApiTest extends TestCase
             [$this->clientFactory->getConfig()->marketplaceId]
         );
 
-        /**
-         * @var ListingsItemSubmissionResponse $responseBody
-         */
-        list($response, $statusCode, $headers) = $result;
-
-        $this->assertEquals($statusCode, 200);
-        $this->assertInstanceOf(ListingsItemSubmissionResponse::class, $response);
-        $this->assertNotEmpty($response->getSubmissionId());
+        $this->_assertSubmissionResponseMatchesStandardExpectations($result);
     }
 
     public function test_patchListingsItem()
     {
         $listingsApi = $this->clientFactory->createListingsItemsApiClient();
 
+        $request = new ListingsItemPatchRequest([
+            'product_type' => 'PRODUCT',
+            'patches'      => [
+                new PatchOperation([
+                    'op'    => 'replace',
+                    'path'  => '/attributes/fulfillment_availability',
+                    'value' => [
+                        [
+                            'fulfillment_channel_code' => 'supply_source_xyz',
+                            'quantity'                 => 5,
+                        ],
+                    ],
+                ]),
+            ],
+        ]);
+
         $result = $listingsApi->patchListingsItemWithHttpInfo(
             $this->clientFactory->getConfig()->sellerId,
             'TESTSKU123',
             [$this->clientFactory->getConfig()->marketplaceId],
-            new ListingsItemPatchRequest()
+            $request
         );
 
-        /**
-         * @var ListingsItemSubmissionResponse $responseBody
-         */
-        list($response, $statusCode, $headers) = $result;
-
-        $this->assertEquals($statusCode, 200);
-        $this->assertInstanceOf(ListingsItemSubmissionResponse::class, $response);
-        $this->assertNotEmpty($response->getSubmissionId());
+        $this->_assertSubmissionResponseMatchesStandardExpectations($result);
     }
 
     public function test_putListingsItem()
     {
         $listingsApi = $this->clientFactory->createListingsItemsApiClient();
 
+        $request = new ListingsItemPutRequest([
+            'product_type' => 'PRODUCT',
+            'requirements' => 'foo',
+            'attributes'   => [
+                'foo'  => 'bar',
+                'fizz' => 'buzz',
+            ],
+        ]);
+
         $result = $listingsApi->putListingsItemWithHttpInfo(
             $this->clientFactory->getConfig()->sellerId,
             'TESTSKU123',
             [$this->clientFactory->getConfig()->marketplaceId],
-            new ListingsItemPutRequest()
+            $request
         );
 
-        /**
-         * @var ListingsItemSubmissionResponse $responseBody
-         */
+        $this->_assertSubmissionResponseMatchesStandardExpectations($result);
+    }
+
+    /**
+     * @param ListingsItemSubmissionResponse|mixed $result
+     * @return void
+     */
+    protected function _assertSubmissionResponseMatchesStandardExpectations($result)
+    {
         list($response, $statusCode, $headers) = $result;
 
         $this->assertEquals($statusCode, 200);
