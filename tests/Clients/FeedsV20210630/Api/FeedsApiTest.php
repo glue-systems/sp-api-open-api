37cb3532851826b8c55a5fc60dc209601b9824f2
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-07-31 09:36:53 +0000

    Adding FeedsV20210630/Api/FeedsApiTest with test for getFeeds

---
+++ tests/Clients/FeedsV20210630/Api/FeedsApiTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Clients\FeedsV20210630\Api;
+
+use Glue\SPAPI\OpenAPI\Clients\FeedsV20210630\Model\Feed;
+use Glue\SPAPI\OpenAPI\Clients\FeedsV20210630\Model\GetFeedsResponse;
+use Glue\SPAPI\OpenAPI\Services\Factory\ClientFactory;
+use Tests\TestCase;
+
+class FeedsApiTest extends TestCase
+{
+    /**
+     * @var ClientFactory
+     */
+    public $clientFactory;
+
+    // TODO: This will need to be changed to `public function setUp(): void` after upgrading.
+    public function setUp()
+    {
+        parent::setup();
+        $this->clientFactory = $this->buildClientFactory();
+    }
+
+    public function test_getFeeds()
+    {
+        $feedsApi  = $this->clientFactory->createFeedsApiV20210630Client();
+
+        $result = $feedsApi->getFeedsWithHttpInfo(
+            // Specific values come from the sandbox spec in models/feeds_2020-09-04.json
+            ['POST_PRODUCT_DATA'],
+            [$this->clientFactory->getConfig()->marketplaceId],
+            10,
+            ['CANCELLED', 'DONE']
+        );
+
+        /**
+         * @var GetFeedsResponse $response
+         */
+        list($response, $statusCode, $headers) = $result;
+
+        $this->assertEquals($statusCode, 200);
+        $this->assertInstanceOf(GetFeedsResponse::class, $response);
+        $this->assertNotEmpty($feeds = $response->getFeeds());
+        $this->assertInstanceOf(Feed::class, $feed = $feeds[0]);
+        $this->assertNotEmpty($feed->getFeedId());
+    }
+}
