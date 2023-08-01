d6e7b48d310286f31b687bed4b4b6f90977d39bb
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-08-01 17:52:41 +0000

    Adding optional Configuration parameters for each create method in ClientFactory; making ClientFactory more DRY with dynamic protected method _createDomainApiClient

--- src/Services/Factory/ClientFactoryContract.php
+++ src/Services/Factory/ClientFactoryContract.php
@@ -3,15 +3,24 @@
 namespace Glue\SPAPI\OpenAPI\Services\Factory;
 
 use Glue\SPAPI\OpenAPI\Clients\DefinitionsProductTypesV20200901\Api\DefinitionsApi as DefinitionsProductTypesV20200901Api;
+use Glue\SPAPI\OpenAPI\Clients\DefinitionsProductTypesV20200901\Configuration as DefinitionsProductTypesV20200901Config;
 use Glue\SPAPI\OpenAPI\Clients\FeedsV20200904\Api\FeedsApi as FeedsV20200904Api;
+use Glue\SPAPI\OpenAPI\Clients\FeedsV20200904\Configuration as FeedsV20200904Config;
 use Glue\SPAPI\OpenAPI\Clients\FeedsV20210630\Api\FeedsApi as FeedsV20210630Api;
+use Glue\SPAPI\OpenAPI\Clients\FeedsV20210630\Configuration as FeedsV20210630Config;
 use Glue\SPAPI\OpenAPI\Clients\ListingsItemsV20200901\Api\ListingsApi as ListingsItemsV20200901Api;
+use Glue\SPAPI\OpenAPI\Clients\ListingsItemsV20200901\Configuration as ListingsItemsV20200901Config;
 use Glue\SPAPI\OpenAPI\Clients\OrdersV0\Api\OrdersV0Api;
 use Glue\SPAPI\OpenAPI\Clients\OrdersV0\Api\ShipmentApi as OrdersV0ShipmentApi;
+use Glue\SPAPI\OpenAPI\Clients\OrdersV0\Configuration as OrdersV0Config;
 use Glue\SPAPI\OpenAPI\Clients\ReportsV20200904\Api\ReportsApi as ReportsV20200904Api;
+use Glue\SPAPI\OpenAPI\Clients\ReportsV20200904\Configuration as ReportsV20200904Config;
 use Glue\SPAPI\OpenAPI\Clients\ReportsV20210630\Api\ReportsApi as ReportsV20210630Api;
+use Glue\SPAPI\OpenAPI\Clients\ReportsV20210630\Configuration as ReportsV20210630Config;
 use Glue\SPAPI\OpenAPI\Clients\SupplySourcesV20200701\Api\SupplySourcesApi as SupplySourcesV20200701Api;
+use Glue\SPAPI\OpenAPI\Clients\SupplySourcesV20200701\Configuration as SupplySourcesV20200701Config;
 use Glue\SPAPI\OpenAPI\Clients\TokensV20210301\Api\TokensApi as TokensV20210301Api;
+use Glue\SPAPI\OpenAPI\Clients\TokensV20210301\Configuration as TokensV20210301Config;
 use Glue\SPAPI\OpenAPI\Clients\TokensV20210301\Model\CreateRestrictedDataTokenRequest;
 use Glue\SPAPI\OpenAPI\Services\SPAPIConfig;
 
@@ -25,49 +34,65 @@ interface ClientFactoryContract
     /**
      * @return SupplySourcesV20200701Api
      */
-    public function createSupplySourcesV20200701ApiClient();
+    public function createSupplySourcesV20200701ApiClient(
+        SupplySourcesV20200701Config $configuration = null
+    );
 
     /**
      * @return ListingsItemsV20200901Api
      */
-    public function createListingsItemsV20200901ApiClient();
+    public function createListingsItemsV20200901ApiClient(
+        ListingsItemsV20200901Config $configuration = null
+    );
 
     /**
      * @return OrdersV0Api
      */
     public function createOrdersV0ApiClient(
+        OrdersV0Config $configuration = null,
         CreateRestrictedDataTokenRequest $rdtRequest = null
     );
 
     /**
      * @return OrdersV0ShipmentApi
      */
-    public function createOrdersV0ShipmentApiClient();
+    public function createOrdersV0ShipmentApiClient(
+        OrdersV0Config $configuration = null
+    );
 
     /**
      * @return DefinitionsProductTypesV20200901Api
      */
-    public function createDefinitionsProductTypesV20200901ApiClient();
+    public function createDefinitionsProductTypesV20200901ApiClient(
+        DefinitionsProductTypesV20200901Config $configuration = null
+    );
 
     /**
      * @return TokensV20210301Api
      */
-    public function createTokensV20210301ApiClient();
+    public function createTokensV20210301ApiClient(
+        TokensV20210301Config $configuration = null
+    );
 
     /**
      * @return FeedsV20200904Api
      */
-    public function createFeedsV20200904ApiClient();
+    public function createFeedsV20200904ApiClient(
+        FeedsV20200904Config $configuration = null
+    );
 
     /**
      * @return FeedsV20210630Api
      */
-    public function createFeedsV20210630ApiClient();
+    public function createFeedsV20210630ApiClient(
+        FeedsV20210630Config $configuration = null
+    );
 
     /**
      * @return ReportsV20200904Api
      */
     public function createReportsV20200904ApiClient(
+        ReportsV20200904Config $configuration = null,
         CreateRestrictedDataTokenRequest $rdtRequest = null
     );
 
@@ -75,6 +100,7 @@ interface ClientFactoryContract
      * @return ReportsV20210630Api
      */
     public function createReportsV20210630ApiClient(
+        ReportsV20210630Config $configuration = null,
         CreateRestrictedDataTokenRequest $rdtRequest = null
     );
 }
