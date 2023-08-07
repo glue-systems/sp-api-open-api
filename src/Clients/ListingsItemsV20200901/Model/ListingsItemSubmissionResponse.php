8054c8bfbfbd7f0a7a761740a67383e069efd10d
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-08-07 20:31:07 +0000

    Updating models/listingsItems_2020-09-01.json with latest changes from official sp-api models GitHub and regenerating client code (only phpdoc changes)

--- src/Clients/ListingsItemsV20200901/Model/ListingsItemSubmissionResponse.php
+++ src/Clients/ListingsItemsV20200901/Model/ListingsItemSubmissionResponse.php
@@ -13,7 +13,7 @@
 /**
  * Selling Partner API for Listings Items
  *
- * The Selling Partner API for Listings Items (Listings Items API) provides programmatic access to selling partner listings on Amazon. Use this API in collaboration with the Selling Partner API for Product Type Definitions, which you use to retrieve the information about Amazon product types needed to use the Listings Items API.
+ * The Selling Partner API for Listings Items (Listings Items API) provides programmatic access to selling partner listings on Amazon. Use this API in collaboration with the Selling Partner API for Product Type Definitions, which you use to retrieve the information about Amazon product types needed to use the Listings Items API.  For more information, see the [Listing Items API Use Case Guide](doc:listings-items-api-v2020-09-01-use-case-guide).
  *
  * OpenAPI spec version: 2020-09-01
  * 
