96ea02ad8c3a59fc1647d6b12c6089de0b845d77
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-08-08 19:40:33 +0000

    Adding AplusContentApiTest

---
+++ tests/Clients/AplusContentV20201101/Api/AplusContentApiTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Clients\AplusContentV20201101\Api;
+
+use Glue\SpApi\OpenAPI\Clients\AplusContentV20201101\ApiException;
+use Glue\SpApi\OpenAPI\Clients\AplusContentV20201101\Model\ContentMetadataRecord;
+use Glue\SpApi\OpenAPI\Clients\AplusContentV20201101\Model\SearchContentDocumentsResponse;
+use Glue\SpApi\OpenAPI\Container\SpApi;
+use GuzzleHttp\Psr7\Stream;
+use Tests\TestCase;
+
+class AplusContentApiTest extends TestCase
+{
+    /**
+     * @var SpApi
+     */
+    public $spApi;
+
+    // TODO: This will need to be changed to `public function setUp(): void` after upgrading.
+    public function setUp()
+    {
+        parent::setup();
+        $this->spApi = $this->buildSpApiContainer();
+    }
+
+    public function test_searchContentDocuments()
+    {
+        $aplusContentApi  = $this->spApi->aplusContentV20201101();
+
+        try {
+            $result = $aplusContentApi->searchContentDocumentsWithHttpInfo(
+                $this->spApi->getSpApiConfig()->marketplaceId
+            );
+        } catch (ApiException $ex) {
+            $response = $ex->getResponseBody();
+            if ($response instanceof Stream) {
+                $contents = $response->getContents();
+            }
+            $this->markTestSkipped('Sandbox not yet available for A+ Content Management API.');
+        }
+
+        /**
+         * @var SearchContentDocumentsResponse $response
+         */
+        list($response, $statusCode, $headers) = $result;
+
+        $this->assertEquals($statusCode, 200);
+        $this->assertInstanceOf(SearchContentDocumentsResponse::class, $response);
+        $this->assertNotEmpty($contentMetadataRecords = $response->getContentMetadataRecords());
+        $this->assertInstanceOf(ContentMetadataRecord::class, $contentMetadataRecords[0]);
+    }
+}
