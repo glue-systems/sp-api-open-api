54afbff0089d73040c4b6b105c41a99e0a536117
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-08-09 03:04:47 +0000

    Adding tests for MerchantFulfillmentV0 and NotificationsV1 api clients

---
+++ tests/Clients/NotificationsV1/Api/NotificationsApiTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Clients\NotificationsV1\Api;
+
+use Glue\SpApi\OpenAPI\Clients\NotificationsV1\ApiException;
+use Glue\SpApi\OpenAPI\Clients\NotificationsV1\Model\GetSubscriptionResponse;
+use Glue\SpApi\OpenAPI\Clients\NotificationsV1\Model\Subscription;
+use Glue\SpApi\OpenAPI\Container\SpApi;
+use GuzzleHttp\Psr7\Stream;
+use Tests\TestCase;
+
+class NotificationsApiTest extends TestCase
+{
+    /**
+     * @var SpApi
+     */
+    public $spApi;
+
+    // TODO: This will need to be changed to `public function setUp(): void` after upgrading.
+    public function setUp()
+    {
+        parent::setup();
+        $this->spApi = $this->buildSpApiContainer();
+    }
+
+    public function test_getSubscription()
+    {
+        $notificationsApi = $this->spApi->notificationsV1();
+
+        $result = $notificationsApi->getSubscriptionWithHttpInfo(
+            'LISTINGS_ITEM_ISSUES_CHANGE'
+        );
+
+        /**
+         * @var GetSubscriptionResponse $response
+         */
+        list($response, $statusCode, $headers) = $result;
+
+        $this->assertEquals($statusCode, 200);
+        $this->assertInstanceOf(GetSubscriptionResponse::class, $response);
+        $this->assertInstanceOf(Subscription::class, $subscription = $response->getPayload());
+        $this->assertNotEmpty($subscription->getDestinationId());
+    }
+}
