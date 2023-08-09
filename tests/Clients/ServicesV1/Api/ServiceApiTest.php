2fb5a835b8b57117b9f793d58640a4c3e8779f75
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-08-09 04:57:53 +0000

    Adding authorization-specific markTestSkipped handling for AuthorizationV1 and ServicesV1 api client tests

--- tests/Clients/ServicesV1/Api/ServiceApiTest.php
+++ tests/Clients/ServicesV1/Api/ServiceApiTest.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace Tests\Clients\AuthorizationV1\Api;
+namespace Tests\Clients\ServicesV1\Api;
 
-use Glue\SpApi\OpenAPI\Clients\AuthorizationV1\ApiException;
-use Glue\SpApi\OpenAPI\Clients\AuthorizationV1\Model\GetAuthorizationCodeResponse;
+use Glue\SpApi\OpenAPI\Clients\ServicesV1\ApiException;
+use Glue\SpApi\OpenAPI\Clients\ServicesV1\Model\CancelServiceJobByServiceJobIdResponse;
 use Glue\SpApi\OpenAPI\Container\SpApi;
 use GuzzleHttp\Psr7\Stream;
 use Tests\TestCase;
 
-class AuthorizationApiTest extends TestCase
+class ServiceApiTest extends TestCase
 {
     /**
      * @var SpApi
@@ -24,13 +24,12 @@ class AuthorizationApiTest extends TestCase
 
     public function test_cancelServiceJobByServiceJobId()
     {
-        $serviceApi = $this->spApi->authorizationV1();
+        $serviceApi = $this->spApi->servicesV1();
 
         try {
-            $result = $serviceApi->getAuthorizationCodeWithHttpInfo(
-                'foo',
-                'foo',
-                'foo'
+            $result = $serviceApi->cancelServiceJobByServiceJobIdWithHttpInfo(
+                'validJobId-48b6d5a3-b708-dbe9-038d-dd95e8d74iut',
+                'V1'
             );
         } catch (ApiException $ex) {
             $body = $ex->getResponseBody();
@@ -44,11 +43,11 @@ class AuthorizationApiTest extends TestCase
         }
 
         /**
-         * @var GetAuthorizationCodeResponse $response
+         * @var CancelServiceJobByServiceJobIdResponse $response
          */
         list($response, $statusCode, $headers) = $result;
 
         $this->assertEquals($statusCode, 200);
-        $this->assertInstanceOf(GetAuthorizationCodeResponse::class, $response);
+        $this->assertInstanceOf(CancelServiceJobByServiceJobIdResponse::class, $response);
     }
 }
