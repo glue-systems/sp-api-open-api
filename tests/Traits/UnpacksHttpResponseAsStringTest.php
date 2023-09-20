c93271746aff0961a55690776df93c76c8181d8f
John Silkey Jr <26827190+jpsilkjr86@users.noreply.github.com>
2023-09-20 20:45:39 +0000

    Completing test coverage for trait UnpacksHttpResponseAsString

--- tests/Traits/UnpacksHttpResponseAsStringTest.php
+++ tests/Traits/UnpacksHttpResponseAsStringTest.php
@@ -7,6 +7,7 @@ use Mockery;
 use Mockery\MockInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
+use stdClass;
 use Tests\TestCase;
 
 class UnpacksHttpResponseAsStringTest extends TestCase
@@ -60,32 +61,61 @@ class UnpacksHttpResponseAsStringTest extends TestCase
 
     public function test_unpackHttpResponseAsString_can_handle_stream_response()
     {
-        $expected = '{ "fake": "stream response body" }';
+        $expectedUnpacked = '{ "fake": "stream response body" }';
         $this->stream->shouldReceive('__toString')
             ->once()
-            ->andReturn($expected);
+            ->andReturn($expectedUnpacked);
 
         $sut = $this;
 
-        $actual = $sut->unpackHttpResponseAsString($this->stream);
+        $actualUnpacked = $sut->unpackHttpResponseAsString($this->stream);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expectedUnpacked, $actualUnpacked);
     }
 
     public function test_unpackHttpResponseAsString_can_handle_generic_response()
     {
-        $expected = '{ "fake": "generic response" }';
+        $expectedUnpacked = '{ "fake": "generic response" }';
         $this->response->shouldReceive('getBody')
             ->once()
             ->andReturn($this->stream);
         $this->stream->shouldReceive('getContents')
             ->once()
-            ->andReturn($expected);
+            ->andReturn($expectedUnpacked);
 
         $sut = $this;
 
-        $actual = $sut->unpackHttpResponseAsString($this->response);
+        $actualUnpacked = $sut->unpackHttpResponseAsString($this->response);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expectedUnpacked, $actualUnpacked);
+    }
+
+    public function test_unpackHttpResponseAsString_can_handle_json_serializable_object_response()
+    {
+        $responseArg      = new stdClass();
+        $responseArg->foo = 'bar';
+
+        $expectedUnpacked = '{"foo":"bar"}';
+
+        $sut = $this;
+
+        $actualUnpacked = $sut->unpackHttpResponseAsString($responseArg);
+
+        $this->assertEquals($expectedUnpacked, $actualUnpacked);
+    }
+
+    public function test_unpackHttpResponseAsString_returns_empty_string_for_non_json_serializable_value_responses()
+    {
+        // Closed resources should return false when passed into json_encode.
+        $responseArg = fopen("{$this->getOutputDirectoryPath()}/temp.txt", "w");
+        fclose($responseArg);
+
+        $expectedUnpacked = '';
+
+        $sut = $this;
+
+        $actualUnpacked = $sut->unpackHttpResponseAsString($responseArg);
+
+        $this->assertEquals($expectedUnpacked, $actualUnpacked);
     }
 }
